28dee61fc819b519851dd90cfefdac4c02ae169f
root <root@localhost.localdomain>
2014-08-19 18:54:52 -0400

    add controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 describe UsersController, type: :controller do
-  
+ 
+  describe "get #new" do
+    before do
+      get :new
+    end
+
+    it "assigns a new user" do
+      expect(assigns(:user)).to be_a_new(User)
+    end
+
+    it "should render :new" do
+      expect(response).to render_template :new
+    end
+
+  end 
 
 end
